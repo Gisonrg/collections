330cba5ee7c47a4474ee1bcf9802910df8f5ed35
Jiang Sheng <gisonrg@gmail.com>
2016-02-03 11:55:11 +0800

    Add filter page for the search.

--- ionicProj1/www/js/controllers/controllers.js
+++ ionicProj1/www/js/controllers/controllers.js
@@ -1,44 +1,55 @@
 angular.module('myApp.controllers', [])
-  .controller('AppCtrl', function($scope) {
+    .controller('AppCtrl', function($scope) {
 
-  })
-  .controller('HomeCtrl', function($scope, $state) {
-    // slider options
-    $scope.options = {
-      loop: true
-    };
-    $scope.data = {};
-    $scope.$watch('data.slider', function(nv, ov) {
-      $scope.slider = $scope.data.slider;
+    })
+    .controller('HomeCtrl', function($scope, $state) {
+        // slider options
+        $scope.options = {
+            loop: true
+        };
+        $scope.data = {};
+        $scope.$watch('data.slider', function(nv, ov) {
+            $scope.slider = $scope.data.slider;
+        });
+    })
+    .controller('CourseListCtrl', function($scope) {
+
+    })
+    .controller('CourseDetailCtrl', function($scope) {
+
+    })
+    .controller('SearchCtrl', function($scope, $ionicModal) {
+        $ionicModal.fromTemplateUrl('filter.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+    })
+    .controller('SearchResultCtrl', function($scope) {
+
+    })
+    .controller('CourseCtrl', function($scope) {
+
+    })
+    .controller('CourseConfirmCtrl', function($scope) {
+
+    })
+    .controller('SettingCtrl', function($scope, $state, $ionicHistory) {
+        $scope.goToProfile = function() {
+            $ionicHistory.nextViewOptions({
+                disableAnimate: true,
+                disableBack: true
+            });
+            $state.go('app.profile');
+        };
+    })
+    .controller('ProfileCtrl', function($scope) {
+        $scope.isLogin = true;
     });
-  })
-  .controller('CourseListCtrl', function($scope) {
-
-  })
-  .controller('CourseDetailCtrl', function($scope) {
-
-  })
-  .controller('SearchCtrl', function($scope) {
-
-  })
-  .controller('SearchResultCtrl', function($scope) {
-
-  })
-  .controller('CourseCtrl', function($scope) {
-
-  })
-  .controller('CourseConfirmCtrl', function($scope) {
-
-  })
-  .controller('SettingCtrl', function($scope, $state, $ionicHistory) {
-    $scope.goToProfile = function() {
-      $ionicHistory.nextViewOptions({
-        disableAnimate: true,
-        disableBack: true
-      });
-      $state.go('app.profile');
-    };
-  })
-  .controller('ProfileCtrl', function($scope) {
-    $scope.isLogin = true;
-  });
